e62b4c16d571b043f0c60aa3300d9153b3a05c52
Pascal Kriete <pascal@Marvin.local>
2008-11-07 07:54:50 +0100

    Added permission library to autoload array.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -39,7 +39,7 @@
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('session');
+$autoload['libraries'] = array('database', 'session', 'permission');
 
 
 /*
